2ad9a8613cbc49a8c23f740e663f41205175c610
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-05-06 19:24:16 +0900

    ResultsViewController の追加

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Anastasia Grey on 2024/05/05.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+}
