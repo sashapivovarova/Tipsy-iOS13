108e6e7bd512421ce853f93f9156062d3d0c6f8e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-05-07 23:00:09 +0900

    CalculatorViewController の修正

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    @IBAction func tipChanged(_ sender: Any) {
+    @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
     }
-    @IBAction func stepperValueChanged(_ sender: Any) {
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
-    @IBAction func calculatePressed(_ sender: Any) {
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
     }
+    
 }
 
